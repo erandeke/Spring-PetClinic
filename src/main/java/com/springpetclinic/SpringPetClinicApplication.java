f2110a38669de5887cfa14520cd68c9b3e616ef7
erandeke <erande.kedar@gmail.com>
2018-12-19 19:04:01 +0530

    Adding Pojo Model

--- src/main/java/com/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/com/springpetclinic/SpringPetClinicApplication.java
@@ -1,12 +1,21 @@
 package com.springpetclinic;
 
+import com.springpetclinic.controller.ResourceController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
+
+		//we asked the context to get the reference of the bean
+		//the spring holds the beanref
+		//This makes no Java instantiation object
+		/*ApplicationContext cts=SpringApplication.run(SpringPetClinicApplication.class, args);
+		ResourceController cont=(ResourceController) cts.getBean("resourceController");
+		cont.Test();*/
 		SpringApplication.run(SpringPetClinicApplication.class, args);
 	}
 
