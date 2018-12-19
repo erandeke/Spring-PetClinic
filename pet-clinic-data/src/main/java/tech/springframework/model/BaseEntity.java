7c32f444f50c2ea243accdbe0db2e3168b3aebad
erandeke <erande.kedar@gmail.com>
2018-12-19 22:28:27 +0530

    Add Base Entity with Long Id. Closes #3

---
+++ pet-clinic-data/src/main/java/tech/springframework/model/BaseEntity.java
@@ -0,0 +1,14 @@
+package tech.springframework.model;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/tech/springframework/model/Person.java
+++ pet-clinic-data/src/main/java/tech/springframework/model/Person.java
@@ -1,6 +1,6 @@
 package tech.springframework.model;
 
-public class Person {
+public class Person  extends  BaseEntity{
 
     private String firstName;
     private String lastName;
