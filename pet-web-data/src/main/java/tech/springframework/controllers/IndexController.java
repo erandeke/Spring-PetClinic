3d8453d48245edb67e944f85bba0f5f66998217e
erandeke <erande.kedar@gmail.com>
2018-12-21 17:14:28 +0530

    Added Controller and Index Page.Closes #6

---
+++ pet-web-data/src/main/java/tech/springframework/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package tech.springframework.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    //Give the list of request
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
